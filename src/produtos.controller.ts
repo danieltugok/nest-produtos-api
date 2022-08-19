b3cebd97cef328d8d918370eb75101fb796992b3
Daniel Kogut <danieltugok@gmail.com>
2022-08-18 20:33:16 -0500

    Creating Controller

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('produtos')
-export class ProdutosController{
-
+export class ProdutosController {
   @Get()
   obterTodos(): string {
     return 'Lista todos os produtos';
@@ -13,4 +12,21 @@ export class ProdutosController{
     return `Retorna os dados do produto ${params.id}`;
   }
 
+  @Post()
+  criar(@Body() produto): string {
+    console.log('produto', produto);
+    return 'Produto criado';
+  }
+
+  @Put()
+  alterar(@Body() produto): string {
+    console.log('produto', produto);
+    return 'Produto atualizado';
+  }
+
+  @Delete(':id')
+  apagar(@Param() params): string {
+    console.log('produto', params.id);
+    return `Apaga o produto ${params.id}`;
+  }
 }
